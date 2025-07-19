b79ef30be8ed89fdf5a10128e961628966638ae0
vincent31545 <vincentlau31545@gmail.com>
2025-07-19 17:18:24 -0400

    pictures to be used

--- src/app/homepage/homepage.tsx
+++ src/app/homepage/homepage.tsx
@@ -1,1 +1,19 @@
-hi
+import "./homepage.css";
+
+export default function HomePage() {
+
+    return (
+        <div className="homepage-container">
+            <div className="homepage-featured-container">
+
+            </div>
+            <div className="homepage-filter-container">
+
+            </div>
+            <div className="homepage-explore-container">
+                
+            </div>
+        </div>
+    )
+}
+
