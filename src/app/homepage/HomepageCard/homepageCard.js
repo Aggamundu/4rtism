3393b9d28914d5b653473f8e55d0d43cb83652eb
vincent31545 <vincentlau31545@gmail.com>
2025-07-19 19:20:53 -0400

    some sort of pintrest explore thing

---
+++ src/app/homepage/HomepageCard/homepageCard.js
@@ -0,0 +1,11 @@
+import "./homepageCard.css";
+
+export default function HomepageCard({ pic, picAlt }) {
+
+    return (
+        <div className="card-container">
+            <img className="card-img" src={ pic } alt={ picAlt } />
+        </div>
+    )
+}
+
